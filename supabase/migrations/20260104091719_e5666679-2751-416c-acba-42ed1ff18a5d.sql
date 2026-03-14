f8aad24baa1931ae3a08639a579e5cddfef8ed61
leroybrothers <gregory@leroybrothers.com>
2026-03-14 17:14:51 +0100

    chore: wipe build artifacts and node_modules

--- supabase/migrations/20260104091719_e5666679-2751-416c-acba-42ed1ff18a5d.sql
+++
@@ -1,4 +0,0 @@
--- Fix: Remove overly permissive SELECT policy on newsletter_subscribers
--- This prevents any authenticated user from reading all subscriber emails
-
-DROP POLICY IF EXISTS "Allow authenticated read" ON public.newsletter_subscribers;
